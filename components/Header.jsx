d026c86c96886267986de5c5aa0663479de35823
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-06-21 00:13:55 -0700

    favicons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,38 @@
+import Head from "next/head";
+
 const Header = () => {
     return (
         <div className="h-16  bg-gray-900 flex-shrink-0">
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta
+                    name="description"
+                    content="The ever changing weather of Victoria, BC"
+                />
+                <title>Victoria Weather</title>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest"></link>
+                <script src="https://www.purpleair.com/pa.widget.js?key=C2U767EE68URYRKH&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_108012_module_AQI_conversion_C0_average_10_layer_standard"></script>
+            </Head>
             <div className=" max-w-3xl mx-auto text-white flex p-2">
                 {/* <div className="py-3 text-4xl inline-block"></div> */}
                 <div className="py-1 px-2 inline-block rounded-md hover:bg-gray-800 transition-colors">
