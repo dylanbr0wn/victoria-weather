fb1b1c6a80a09a53313e62b81cf475cca6990b5f
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-06-26 20:24:38 -0700

    fix missing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts,jsx,tsx}"
     ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
