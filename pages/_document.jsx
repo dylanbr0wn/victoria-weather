8c987448aa733b9998e5093c9bbf42af31bdc43a
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-07-26 23:15:51 -0700

    change to secure

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link
-					href="http://fonts.cdnfonts.com/css/aileron?styles=20898,20896,20899,20891,20893"
+					href="https://fonts.cdnfonts.com/css/aileron?styles=20898,20896,20899,20891,20893"
 					rel="stylesheet"
 				/>
 			</Head>
