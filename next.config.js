9bdc05bb756efa9fa07849859ce2f2fb2e7ff0c9
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-07-27 21:34:15 -0700

    axiom with reporting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const { withAxiom } = require("next-axiom");
+
+const nextConfig = withAxiom({
 	reactStrictMode: true,
 	swcMinify: true,
-};
+});
 
 module.exports = nextConfig;
