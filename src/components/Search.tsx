7e73fe87b85a62226ef54fa66f4bccd181a2fe11
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-07-28 00:20:44 -0700

    fixes react errors

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,16 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getPointsData } from "../utils/apiGetters";
 import { Point } from "../utils/types";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Search = () => {
 	const [search, setSearch] = useState("");
 
 	const [results, setResults] = useState<
 		// eslint-disable-next-line no-undef
-		GeoJSON.Feature<
-			Point,
-			{
-				[name: string]: any;
-			}
-		>[]
+		GeoJSON.Feature<Point>[]
 	>([]);
 	const [searchLength, setSearchLength] = useState(4);
 
@@ -22,24 +18,28 @@ const Search = () => {
 
 	useEffect(() => {
 		if (data?.points?.points) {
-			let newResults = data?.points?.points.features.filter((point) => {
-				if (search.length === 0) return true;
+			let newResults = data?.points?.points.features
+				.filter((point) => {
+					if (search.length === 0) return true;
 
-				const result = point.properties.station_long_name
-					.toLowerCase()
-					.search(search.toLowerCase());
-				return result >= 0;
-			});
+					const result = point.properties.station_long_name
+						.toLowerCase()
+						.search(search.toLowerCase());
+					return result >= 0;
+				})
+				.slice(0, Math.min(data?.points?.points.features.length, searchLength));
 
 			setResults(newResults);
 		}
-	}, [data, search]);
+	}, [data, search, searchLength]);
 
 	const onChange = ({ target }) => {
 		if (target.name === "search") {
 			setSearch(target.value);
 		}
 	};
+
+	const [animationParent] = useAutoAnimate<HTMLDivElement>();
 	return (
 		<>
 			{data?.points?.points && (
@@ -76,156 +76,129 @@ const Search = () => {
 						</div>
 					</div>
 
-					<div className="mt-3">
-						<AnimatePresence exitBeforeEnter initial={false}>
-							{results
-								.slice(0, Math.min(results.length, searchLength))
-								.map(({ properties, geometry }) => {
-									const coordinates = `${geometry.coordinates[1].toFixed(
-										7
-									)}, ${geometry.coordinates[0].toFixed(7)}`;
+					<div className="mt-3" ref={animationParent}>
+						{results.map(({ properties, geometry }) => {
+							const coordinates = `${geometry.coordinates[1].toFixed(
+								7
+							)}, ${geometry.coordinates[0].toFixed(7)}`;
 
-									const temperature =
-										properties.temperature?.toFixed(1) || null;
-									const rain = properties.rain?.toFixed(1) || null;
-									const elevation = properties.elevation || null;
+							const temperature = properties.temperature || null;
+							const rain = properties.rain?.toFixed(1) || null;
+							const elevation = properties.elevation || null;
 
-									const pressure = properties.pressure
-										? `${properties.pressure} ${properties.pressure_units}`
-										: null;
+							const pressure = properties.pressure
+								? `${properties.pressure} ${properties.pressure_units}`
+								: null;
 
-									const humidity = properties.humidity
-										? `${properties.humidity} ${properties.humidity_units}`
-										: null;
+							// const humidity = properties.
+							// 	? `${properties.humidity} ${properties.humidity_units}`
+							// 	: null;
 
-									return (
-										<motion.a
-											initial={{ opacity: 0 }}
-											animate={{ opacity: 1 }}
-											exit={{ opacity: 0 }}
-											transition={{ duration: 0.2 }}
-											key={properties.station_id}
-											className="w-full my-1 p-3 flex flex-col hover:bg-gray-800 rounded-md transition-colors"
-											rel="noopener noreferrer"
-											target="_blank"
-											href={`https://www.victoriaweather.ca/station.php?id=${properties.station_id}`}
-										>
-											<div className="flex">
-												<div className="text-white text-lg font-black tracking-wider ml-3">
-													{properties.station_long_name}
-												</div>
-											</div>
-											<div className="flex flex-wrap py-1">
-												<div className="text-gray-500 text-xs ml-3 mr-1">
-													📍
-												</div>
-												<div className="text-gray-500 text-xs">
-													{coordinates}
-												</div>
-												{elevation && (
-													<>
-														<div className="text-gray-500 text-xs ml-3 mr-1">
-															⬆
-														</div>
-														<div className="text-gray-500 text-xs">
-															{elevation}m
-														</div>
-													</>
-												)}
-
-												{temperature && (
-													<>
-														<div className="text-gray-500 text-xs ml-3 mr-1">
-															🌡
-														</div>
-														<div
-															className={` text-xs ${
-																temperature < 10
-																	? "text-blue-500"
-																	: temperature < 20
-																	? "text-yellow-400"
-																	: temperature < 30
-																	? "text-green-500"
-																	: "text-red-600"
-															}`}
-														>
-															{temperature}℃
-														</div>
-													</>
-												)}
-												{rain && (
-													<>
-														<div className="text-gray-500 text-xs ml-3 mr-1">
-															💧
-														</div>
-														<div
-															style={{
-																color: "#30B4FF",
-															}}
-															className=" text-xs"
-														>
-															{rain}mm
-														</div>
-													</>
-												)}
-												{pressure && (
-													<>
-														<div className="text-gray-500 text-xs ml-3 mr-1">
-															☁️
-														</div>
-														<div className=" text-xs text-white">
-															{pressure}
-														</div>
-													</>
-												)}
-												{humidity && (
-													<>
-														<div className="text-gray-500 text-xs ml-3 mr-1">
-															💦
-														</div>
-														<div
-															style={{
-																color: "#30E6FF",
-															}}
-															className=" text-xs"
-														>
-															{humidity}
-														</div>
-													</>
-												)}
-											</div>
-										</motion.a>
-									);
-								})}
-							{results.length === 0 && (
-								<motion.div
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									exit={{ opacity: 0 }}
-									key="-1"
-									className="w-full text-center font-black text-gray-500 p-5 text-lg"
+							return (
+								<a
+									key={properties.station_id}
+									className="w-full my-1 p-3 flex flex-col hover:bg-gray-800 rounded-md transition-colors"
+									rel="noopener noreferrer"
+									target="_blank"
+									href={`https://www.victoriaweather.ca/station.php?id=${properties.station_id}`}
 								>
-									<span className="italic text-xl">Impossible...</span>
-									<br /> Perhaps the archives are incomplete!
-								</motion.div>
-							)}
-						</AnimatePresence>
-					</div>
-					<AnimatePresence initial={false}>
-						{results.length > searchLength && (
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
+									<div className="flex">
+										<div className="text-white text-lg font-black tracking-wider ml-3">
+											{properties.station_long_name}
+										</div>
+									</div>
+									<div className="flex flex-wrap py-1">
+										<div className="text-gray-500 text-xs ml-3 mr-1">📍</div>
+										<div className="text-gray-500 text-xs">{coordinates}</div>
+										{elevation && (
+											<>
+												<div className="text-gray-500 text-xs ml-3 mr-1">⬆</div>
+												<div className="text-gray-500 text-xs">
+													{elevation}m
+												</div>
+											</>
+										)}
+
+										{temperature && (
+											<>
+												<div className="text-gray-500 text-xs ml-3 mr-1">🌡</div>
+												<div
+													className={` text-xs ${
+														temperature < 10
+															? "text-blue-500"
+															: temperature < 20
+															? "text-yellow-400"
+															: temperature < 30
+															? "text-green-500"
+															: "text-red-600"
+													}`}
+												>
+													{temperature.toFixed(1)}℃
+												</div>
+											</>
+										)}
+										{rain && (
+											<>
+												<div className="text-gray-500 text-xs ml-3 mr-1">
+													💧
+												</div>
+												<div
+													style={{
+														color: "#30B4FF",
+													}}
+													className=" text-xs"
+												>
+													{rain}mm
+												</div>
+											</>
+										)}
+										{pressure && (
+											<>
+												<div className="text-gray-500 text-xs ml-3 mr-1">
+													☁️
+												</div>
+												<div className=" text-xs text-white">{pressure}</div>
+											</>
+										)}
+										{/* {humidity && (
+											<>
+												<div className="text-gray-500 text-xs ml-3 mr-1">
+													💦
+												</div>
+												<div
+													style={{
+														color: "#30E6FF",
+													}}
+													className=" text-xs"
+												>
+													{humidity}
+												</div>
+											</>
+										)} */}
+									</div>
+								</a>
+							);
+						})}
+						{results.length === 0 && (
+							<div
+								key="-1"
+								className="w-full text-center font-black text-gray-500 p-5 text-lg"
+							>
+								<span className="italic text-xl">Impossible...</span>
+								<br /> Perhaps the archives are incomplete!
+							</div>
+						)}
+						{data.points.points.features.length > searchLength && (
+							<div
 								onClick={() => {
 									setSearchLength(searchLength * 2);
 								}}
-								className="inline-block text-center hover:underline cursor-pointer text-gray-500 p-5"
+								className="w-full inline-block text-center hover:underline cursor-pointer text-gray-500 p-5"
 							>
 								show more results
-							</motion.div>
+							</div>
 						)}
-					</AnimatePresence>
+					</div>
 				</motion.div>
 			)}
 		</>
