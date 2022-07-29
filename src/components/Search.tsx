6e2c45324305a6c54f01928401c75d63bd408d0d
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-07-28 18:45:44 -0700

    remove use effect dependence

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,202 +1,195 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getPointsData } from "../utils/apiGetters";
-import { Point } from "../utils/types";
+import { Point, PointsData } from "../utils/types";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+// eslint-disable-next-line no-undef
+type PointFeatureArray = GeoJSON.Feature<Point>[];
+
+const filter = (data: PointsData, term: string): PointFeatureArray => {
+	console.log("filter", term);
+	if (!data?.points?.points) return [];
+	return data?.points?.points.features.filter((point) => {
+		if (term.length === 0) return true;
+
+		const name: string = point.properties.station_long_name;
+
+		const result = name.toLowerCase().search(term.toLowerCase());
+		return result >= 0;
+	});
+};
+
+const truncate = (
+	data: PointFeatureArray,
+	resLength: number,
+	totalLength: number
+) => {
+	return data.slice(0, Math.min(resLength, totalLength));
+};
 
 const Search = () => {
 	const [search, setSearch] = useState("");
-
-	const [results, setResults] = useState<
-		// eslint-disable-next-line no-undef
-		GeoJSON.Feature<Point>[]
-	>([]);
+	const [resultsLength, setResultsLength] = useState<number>(0);
+	const [results, setResults] = useState<PointFeatureArray>([]);
 	const [searchLength, setSearchLength] = useState(4);
 
-	const { data } = useQuery(["points"], getPointsData, {});
+	const { data } = useQuery(["points"], getPointsData, {
+		onSuccess: (data) => {
+			if (data?.points?.points) {
+				getResults(data, search, searchLength);
+			}
+		},
+	});
 
-	useEffect(() => {
-		if (data?.points?.points) {
-			let newResults = data?.points?.points.features
-				.filter((point) => {
-					if (search.length === 0) return true;
+	const getResults = (pointsData: PointsData, term: string, len: number) => {
+		const results = filter(pointsData, term);
+		setResultsLength(results.length);
+		setResults(
+			truncate(results, len, pointsData?.points.points.features.length)
+		);
+	};
 
-					const result = point.properties.station_long_name
-						.toLowerCase()
-						.search(search.toLowerCase());
-					return result >= 0;
-				})
-				.slice(0, Math.min(data?.points?.points.features.length, searchLength));
-
-			setResults(newResults);
-		}
-	}, [data, search, searchLength]);
-
-	const onChange = ({ target }) => {
+	const onSearch = ({ target }) => {
 		if (target.name === "search") {
 			setSearch(target.value);
+			getResults(data, target.value, searchLength);
 		}
 	};
 
 	const [animationParent] = useAutoAnimate<HTMLDivElement>();
 	return (
 		<>
-			{data?.points?.points && (
-				<div
-					style={{ minHeight: "28rem" }}
-					className="px-10 py-5 w-full flex flex-col"
-				>
-					<div className="relative h-10 w-full">
-						<input
-							type="text"
-							placeholder="Search for a station..."
-							value={search}
-							onChange={onChange}
-							name="search"
-							className="bg-gray-800 border-none rounded-full w-full h-full caret-white pr-5 pl-10 text-white focus:border-gray-500 transition-color focus:ring-gray-500"
-						/>
-						<div className="absolute h-full w-full flex top-0 items-center pointer-events-none">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5 text-gray-400 ml-3"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fillRule="evenodd"
-									d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-									clipRule="evenodd"
-								/>
-							</svg>
-						</div>
-					</div>
-
-					<div className="mt-3" ref={animationParent}>
-						{results.map(({ properties, geometry }) => {
-							const coordinates = `${geometry.coordinates[1].toFixed(
-								7
-							)}, ${geometry.coordinates[0].toFixed(7)}`;
-
-							const temperature = properties.temperature || null;
-							const rain = properties.rain?.toFixed(1) || null;
-							const elevation = properties.elevation || null;
-
-							const pressure = properties.pressure
-								? `${properties.pressure} ${properties.pressure_units}`
-								: null;
-
-							// const humidity = properties.
-							// 	? `${properties.humidity} ${properties.humidity_units}`
-							// 	: null;
-
-							return (
-								<a
-									key={properties.station_id}
-									className="w-full my-1 p-3 flex flex-col hover:bg-gray-800 rounded-md transition-colors"
-									rel="noopener noreferrer"
-									target="_blank"
-									href={`https://www.victoriaweather.ca/station.php?id=${properties.station_id}`}
-								>
-									<div className="flex">
-										<div className="text-white text-lg font-black tracking-wider ml-3">
-											{properties.station_long_name}
-										</div>
-									</div>
-									<div className="flex flex-wrap py-1">
-										<div className="text-gray-500 text-xs ml-3 mr-1">📍</div>
-										<div className="text-gray-500 text-xs">{coordinates}</div>
-										{elevation && (
-											<>
-												<div className="text-gray-500 text-xs ml-3 mr-1">⬆</div>
-												<div className="text-gray-500 text-xs">
-													{elevation}m
-												</div>
-											</>
-										)}
-
-										{temperature && (
-											<>
-												<div className="text-gray-500 text-xs ml-3 mr-1">🌡</div>
-												<div
-													className={` text-xs ${
-														temperature < 10
-															? "text-blue-500"
-															: temperature < 20
-															? "text-yellow-400"
-															: temperature < 30
-															? "text-green-500"
-															: "text-red-600"
-													}`}
-												>
-													{temperature.toFixed(1)}℃
-												</div>
-											</>
-										)}
-										{rain && (
-											<>
-												<div className="text-gray-500 text-xs ml-3 mr-1">
-													💧
-												</div>
-												<div
-													style={{
-														color: "#30B4FF",
-													}}
-													className=" text-xs"
-												>
-													{rain}mm
-												</div>
-											</>
-										)}
-										{pressure && (
-											<>
-												<div className="text-gray-500 text-xs ml-3 mr-1">
-													☁️
-												</div>
-												<div className=" text-xs text-white">{pressure}</div>
-											</>
-										)}
-										{/* {humidity && (
-											<>
-												<div className="text-gray-500 text-xs ml-3 mr-1">
-													💦
-												</div>
-												<div
-													style={{
-														color: "#30E6FF",
-													}}
-													className=" text-xs"
-												>
-													{humidity}
-												</div>
-											</>
-										)} */}
-									</div>
-								</a>
-							);
-						})}
-						{results.length === 0 && (
-							<div
-								key="empty"
-								className="w-full text-center font-black text-gray-500 p-5 text-lg"
-							>
-								<span className="italic text-xl">Impossible...</span>
-								<br /> Perhaps the archives are incomplete!
-							</div>
-						)}
-						{data.points.points.features.length > searchLength && (
-							<div
-								key="show-more"
-								onClick={() => {
-									setSearchLength(searchLength * 2);
-								}}
-								className="w-full inline-block text-center hover:underline cursor-pointer text-gray-500 p-5"
-							>
-								show more results
-							</div>
-						)}
+			<div
+				style={{ minHeight: "28rem" }}
+				className="px-10 py-5 w-full flex flex-col"
+			>
+				<div className="relative h-10 w-full">
+					<input
+						type="text"
+						placeholder="Search for a station..."
+						value={search}
+						onChange={onSearch}
+						name="search"
+						className="bg-gray-800 border-none rounded-full w-full h-full caret-white pr-5 pl-10 text-white focus:border-gray-500 transition-color focus:ring-gray-500"
+					/>
+					<div className="absolute h-full w-full flex top-0 items-center pointer-events-none">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5 text-gray-400 ml-3"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fillRule="evenodd"
+								d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+								clipRule="evenodd"
+							/>
+						</svg>
 					</div>
 				</div>
-			)}
+
+				<div className="mt-3" ref={animationParent}>
+					{results.map(({ properties, geometry }) => {
+						const coordinates = `${geometry.coordinates[1].toFixed(
+							7
+						)}, ${geometry.coordinates[0].toFixed(7)}`;
+
+						const temperature = properties.temperature || null;
+						const rain = properties.rain?.toFixed(1) || null;
+						const elevation = properties.elevation || null;
+
+						const pressure = properties.pressure
+							? `${properties.pressure} ${properties.pressure_units}`
+							: null;
+
+						// const humidity = properties.
+						// 	? `${properties.humidity} ${properties.humidity_units}`
+						// 	: null
+
+						return (
+							<a
+								key={properties.station_id}
+								className="w-full my-1 p-3 flex flex-col hover:bg-gray-800 rounded-md transition-colors"
+								rel="noopener noreferrer"
+								target="_blank"
+								href={`https://www.victoriaweather.ca/station.php?id=${properties.station_id}`}
+							>
+								<div className="flex">
+									<div className="text-white text-lg font-black tracking-wider ml-3">
+										{properties.station_long_name}
+									</div>
+								</div>
+								<div className="flex flex-wrap py-1">
+									<div className="text-gray-500 text-xs ml-3 mr-1">📍</div>
+									<div className="text-gray-500 text-xs">{coordinates}</div>
+									{elevation && (
+										<>
+											<div className="text-gray-500 text-xs ml-3 mr-1">⬆</div>
+											<div className="text-gray-500 text-xs">{elevation}m</div>
+										</>
+									)}
+
+									{temperature && (
+										<>
+											<div className="text-gray-500 text-xs ml-3 mr-1">🌡</div>
+											<div
+												className={` text-xs ${
+													temperature < 10
+														? "text-blue-500"
+														: temperature < 20
+														? "text-yellow-400"
+														: temperature < 30
+														? "text-green-500"
+														: "text-red-600"
+												}`}
+											>
+												{temperature.toFixed(1)}℃
+											</div>
+										</>
+									)}
+									{rain && (
+										<>
+											<div className="text-gray-500 text-xs ml-3 mr-1">💧</div>
+											<div
+												style={{
+													color: "#30B4FF",
+												}}
+												className=" text-xs"
+											>
+												{rain}mm
+											</div>
+										</>
+									)}
+									{pressure && (
+										<>
+											<div className="text-gray-500 text-xs ml-3 mr-1">☁️</div>
+											<div className=" text-xs text-white">{pressure}</div>
+										</>
+									)}
+								</div>
+							</a>
+						);
+					})}
+					{resultsLength === 0 && (
+						<div className="w-full text-center font-black text-gray-500 p-5 text-lg">
+							<span className="italic text-xl">Impossible...</span>
+							<br /> Perhaps the archives are incomplete!
+						</div>
+					)}
+					{resultsLength > searchLength && (
+						<div
+							onClick={() => {
+								setSearchLength(searchLength * 2);
+								getResults(data, search, searchLength * 2);
+							}}
+							className="w-full inline-block text-center hover:underline cursor-pointer text-gray-500 p-5"
+						>
+							show more results
+						</div>
+					)}
+				</div>
+			</div>
 		</>
 	);
 };
